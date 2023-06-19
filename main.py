b8c06c291123f43a9866832c67c2de3776bf85e0
vamshi336 <69507106+vamshi336@users.noreply.github.com>
2023-06-19 05:54:05 +0000

    added some changes

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from database import load_stats_from_db
 
 app = Flask(__name__)
-RECORDS = [{
-  'player': 'kholi',
-  'runs': 200,
-  'mtches': 240
-}, {
-  'player': 'rohith',
-  'runs': 400,
-  'mtches': 240
-}]
+
 Bets = ['india', 'pak', 'sa', 'nz']
 
 
@@ -18,6 +10,58 @@ Bets = ['india', 'pak', 'sa', 'nz']
 def hallo():
   RECORDS = load_stats_from_db()
   return render_template('home.html', records=RECORDS, bets=Bets)
+
+
+@app.route("/wallet")
+def wallet():
+  return '''
+  <html>
+  <head>
+  helo
+  </head>
+  </html>
+'''
+
+
+@app.route('/sinin', methods=['POST', 'GET'])
+def button_clicked():
+  # Handle button click logic here
+  return render_template('sinin.html')
+
+
+@app.route('/sinup', methods=['POST', 'GET'])
+def sinup():
+  # Handle button click logic here
+  return render_template('sinup.html')
+
+
+@app.route('/sinup/s', methods=['POST'])
+def signup():
+  # Retrieve form data
+  username = request.form['username']
+  email = request.form['email']
+  password = request.form['password']
+
+  # Create a cursor object to execute queries
+  cursor = db.cursor()
+
+  # Execute the query to insert user data into the database
+  query = "INSERT INTO Users (username, email, password) VALUES (%s, %s, %s)"
+  values = (username, email, password)
+  cursor.execute(query, values)
+
+  # Commit the changes to the database
+  db.commit()
+
+  # Close the cursor
+  cursor.close()
+
+  return "Registration successful!"
+
+
+@app.route('/admin')
+def gobackhome():
+  return redirect(url_for("hallo"))
 
 
 if __name__ == '__main__':
