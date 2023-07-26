f10b9525654e99f86336d4bdedf3e06e40c5ae9d
vamshi336 <69507106+vamshi336@users.noreply.github.com>
2023-07-26 07:17:07 +0000

    added bets histry  requirement

--- database.py
+++ database.py
@@ -10,18 +10,18 @@ engine = create_engine(my_secret,
                          "ssl_ca": "/etc/ssl/cert.pem"
                        }})
 
-def get_bal_from_wallet(username):
-    with engine.connect() as conn:
-        query = text("SELECT balance FROM Users WHERE Username=:val")
-        result = conn.execute(query, {'val': username})
-        row = result.fetchone()
-        if row is not None:
-            balance = row[0]  # Access the balance column of the retrieved row
-            return balance
-        else:
-            return None  # Return None if the user is not found or balance is not available
 
-  
+def get_bal_from_wallet(username):
+  with engine.connect() as conn:
+    query = text("SELECT balance FROM Users WHERE Username=:val")
+    result = conn.execute(query, {'val': username})
+    row = result.fetchone()
+    if row is not None:
+      balance = row[0]  # Access the balance column of the retrieved row
+      return balance
+    else:
+      return None  # Return None if the user is not found or balance is not available
+
 
 def load_stats_from_db():
   with engine.connect() as conn:
@@ -50,11 +50,14 @@ def get_stats_from_db(id=None):
       return rows[0]
 
 
-def add_betto_db(amount, player_name, r):
-  query = "INSERT INTO bettable (amount, P_name,ratio) VALUES (:b_amount, :p_name,:b_ratio)"
+def add_betto_db(username, amount, player_name, r):
+  query = "INSERT INTO bettable (username,amount, P_name,ratio) VALUES (:u_name,:b_amount, :p_name,:b_ratio)"
   with engine.connect() as conn:
     conn.execute(
-      text(query).params(b_amount=amount, p_name=player_name, b_ratio=r))
+      text(query).params(u_name=username,
+                         b_amount=amount,
+                         p_name=player_name,
+                         b_ratio=r))
 
 
 def add_newuser_to_db(username, email, password):
@@ -98,4 +101,11 @@ def get_users_data(username):
   return None
 
 
-
+def user_bets(username):
+  print("Username:", username)
+  with engine.connect() as conn:
+    query = text('select * from bettable where username=:val')
+    result = conn.execute(query, {'val': username})
+    row = result.fetchone()
+    print("Query Result:", row)  # Check the query result
+  return row
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
-from database import load_stats_from_db, get_stats_from_db, row_to_dict, add_betto_db, add_newuser_to_db, get_users_data, get_bal_from_wallet
+from database import load_stats_from_db, get_stats_from_db, row_to_dict, add_betto_db, add_newuser_to_db, get_users_data, get_bal_from_wallet, user_bets
 from flask import jsonify
 from flask_login import login_required, LoginManager
 from flask_login import login_user
@@ -18,8 +18,8 @@ app.secret_key = secret_key
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_PERMANENT'] = True
 app.config['SESSION_USE_SIGNER'] = True
-app.config['SESSION_KEY_PREFIX'] = 'your_prefix_here'  # Change this to a unique prefix
-
+app.config[
+  'SESSION_KEY_PREFIX'] = 'your_prefix_here'  # Change this to a unique prefix
 
 
 class User:
@@ -55,29 +55,30 @@ def button_clicked():
 # Define a global variable to store the username
 global_username = None
 
+
 @app.route('/signinsuccess', methods=['POST', 'GET'])
 def success():
-    global global_username
+  global global_username
 
-    username = request.form['username']
-    password = request.form['password']
+  username = request.form['username']
+  password = request.form['password']
 
-    user_data = get_users_data(username)
+  user_data = get_users_data(username)
 
-    if user_data and password == user_data['password']:
-        user_id = username
-        user = User(user_id, user_data['username'], user_data['email'],
-                    user_data['password'])
-        login_user(user)
+  if user_data and password == user_data['password']:
+    user_id = username
+    user = User(user_id, user_data['username'], user_data['email'],
+                user_data['password'])
+    login_user(user)
 
-        # Store the username in the global variable
-        global_username = username
+    # Store the username in the global variable
+    global_username = username
 
-        return redirect(url_for('hallo'))
-    else:
-        return "Invalid username or password"
+    return redirect(url_for('hallo'))
+  else:
+    return "Invalid username or password"
 
-    
+
 @login_manager.user_loader
 def load_user(user_id):
   # Load the user from the user_id (username in your case)
@@ -146,10 +147,22 @@ def apply_to_jobs(id):
   total_data_of_player = get_stats_from_db(id)
   player_name = total_data_of_player[1]
   r = total_data_of_player[5]
-  amount = data.get('amount')  # Extract the 'amount' value from the dictionary
-  add_betto_db(amount, player_name,
-               r)  # Pass the 'amount' value as an argument
-  return render_template('betplaced.html', DATA=data)
+  amount = int(
+    data.get('amount', 0)
+  )  # Convert 'amount' to an integer, default to 0 if not presentand # Extract the 'amount' value from the dictionary
+  user_balance = get_bal_from_wallet(global_username)
+  if user_balance >= amount:
+    add_betto_db(global_username, amount, player_name,
+                 r)  # Pass the 'amount' value as an argument
+    return render_template('betplaced.html', DATA=data)
+  else:
+    return 'plese add amount.Your current balance is less than bet amount'
+
+
+@app.route('/bets', methods=['GET'])
+def get_bets():
+  x = user_bets(global_username)
+  return render_template("bets.html", x=x)
 
 
 if __name__ == '__main__':
